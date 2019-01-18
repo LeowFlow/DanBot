790b189f11058930c5dacc83e4b0e05af4c9f104
danielpmc <danielpd93@gmail.com>
2019-01-18 16:28:02 +0000

    Automatic - Date: 18/01/2019 - Time: 16:28:01.78

--- commands/ban.js
+++ commands/ban.js
@@ -1,44 +1,65 @@
-const { RichEmbed } = require('discord.js');
+const Discord = require("discord.js")
+const sql = require('sqlite3');
+const fs = require('fs');
 
-exports.run = async(client, message, args, db) => {
+//New Ban Stuff With Random Case IDs!
+let Pcount = JSON.parse(fs.readFileSync("./datajsons/Pcount.json", "utf8"));
+module.exports.run = async (client, message, args) => {
+      const settings = message.settings;
+      const defaults = client.config.defaultSettings;
+      const overrides = client.settings.get(message.guild.id);
+      if (!client.settings.has(message.guild.id)) client.settings.set(message.guild.id, {});
 
-    if (args.length < 1) return message.channel.send("\:x: Invalid Arguments. `" + client.config.prefix + "ban <@User || ID>`");
 
-    let guildMember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
-    let reason = args.slice(1).join(" ");
-    let user = client.users.get(guildMember.id)
+      //let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      let bUser = message.mentions.users.first();
+      let bReason = args.slice(1).join(" ")
+      if(!bUser) return message.channel.send("Can't find that user!");
+      if(!bUser) return message.channel.send("Please mention someone to ban!");
+      if(bReason.length < 1) return message.reply("Please provide a reason for this ban.")
+      //let bReason = args.join(" ").slice(22);
 
-    let action;
-    if (user.bot){
-        action = 'Bot Ban'
-    }else{
-        action = 'User Ban'
-    }
+      message.delete()
+      message.channel.send(`Banned ${bUser} for ${bReason}. <@${message.author.id}>`)
+      let banEmbed = new Discord.RichEmbed()
+      .setDescription("**__DanBot - New Ban!__**")
+      .setColor("#ff0011")
+      .addField("ID:", `${Pcount[message.guild.id].BC}`)
+      .addField("**User**", `${bUser}`, true)
+      .addField("**Moderator**", `<@${message.author.id}>`, true)
+      .addField("**Reason**", bReason)
+      .setTimestamp(new Date);
 
-    db.run(`INSERT INTO actions(user, action, mod) VALUES(?, ?, ?)`, [user.tag, action, message.author.tag], function(err) {
-        if (err) {
-          return message.channel.send("\:warning: An unexpected database error occurred.")
-        }
-        guildMember.ban('Bot Ban.');
-        message.channel.send(`\:white_check_mark: **${user.username}** has been banned. Check <#${client.config.modLogs}>!`) 
-      const embed = new RichEmbed()
-      .setTitle(action + " | Case #" + this.lastID)
-      .setColor("#7ED321")
-      .addField("User", user.tag, true)
-      .addField("Moderator", message.author.tag, true)
-      .addField("Reason", "`" + client.config.prefix + "reason " + this.lastID + " [Reason]`")
-      client.channels.get(client.config.modLogs).send({embed}).then(m => 
+      const uembed = new Discord.RichEmbed()
+      .setTitle("**__DanBot Ban__**")
+      .setColor("#ff0000")
+      .setDescription(`You got banned from **${message.guild.name}**`)
+      .addField("Moderator:", message.author.tag)
+      .addField("Reason:", bReason)
+      .setTimestamp(new Date);
 
-        db.run(`UPDATE actions SET messageID = ? WHERE id = ?`, [m.id, this.lastID], function(err) {
-          if (err) {
-            return message.channel.send("\:warning: An unexpected database error occurred.")
-          }
-         
-        })
-    )
-})
-
+if(!Pcount[message.guild.id]) Pcount[message.guild.id] = {
+  KC: 0,
+  BC: 0
 }
+
+
+      Pcount[message.guild.id].BC ++;
+      Pcount.BC ++;
+      fs.writeFile("./datajsons/Pcount.json", JSON.stringify(Pcount), (err) => {
+         if (err) console.log(err)
+       });
+
+      let banChannel = message.guild.channels.find(`name`, `${settings.modLogChannel}`);
+      var server = message.guild;
+      if(!banChannel) return message.channel.send(`Cant find the channel: ${settings.modLogChannel}`);
+
+      message.guild.member(bUser).send({embed: uembed})
+      message.guild.member(bUser).ban(bReason);
+      banChannel.send(banEmbed);
+
+      return;
+      }
 
       exports.conf = {
             enabled: true,
