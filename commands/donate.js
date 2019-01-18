2fb2d2bc2de314c2975a3da5d6c16f6676badb8d
danielpmc <danielpd93@gmail.com>
2019-01-18 12:24:55 +0000

    Jan 18th 2019 | 12:24pm

--- commands/donate.js
+++ commands/donate.js
@@ -7,12 +7,12 @@ Hi I'm danielpmc the owner of DanBot! Me and my team spent months create Danbot 
 Maintaining this bot is not Free, we need to pay the server, the domain for the website, etc...
 we do not want to be paid to code, we just need money to maintain our bot.\nIf you want to support us and DanBot. Please donate using paypal.
 Thank You for using DanBot
-All the donations are used for System and the dashboard that u can access by pressing [**HERE**](http://www.danbot.xyz/). 
+All the donations are used for System and the dashboard that u can access by pressing [**__HERE__**](http://www.danbot.xyz/). 
 All donations above 1$ we will give something back like the donators perm level on DanBot where you will soon have access to commands that normal users wont.
 And you will be on \`!donators\`
 
 
-[**Paypal**](https://www.paypal.me/danielpmc)`,
+[**__Paypal__**](https://www.paypal.me/danielpmc)`,
 			color: 0xFFFFFF
 		}
 	});
