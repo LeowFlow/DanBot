ec1ef8a4465fc6f7cfa33a75e0b631a2f7655866
danielpmc <danielpd93@gmail.com>
2019-01-18 12:51:14 +0000

    Automatic

--- commands/msgleaderboard.js
+++ commands/msgleaderboard.js
@@ -1,15 +1,6 @@
 exports.run = async (client, message, args, level) => {
-    const execSync = require('child_process').execSync;
-      const code = args.join(" ");
-      try {
-        const evaled = eval(code);
-        const clean = await client.clean(client, evaled);
-        const say = message.channel.send()
-        message.delete();
-        message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
-      } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
-      }
+    let mlb = JSON.parse(fs.readFileSync("./datajsons/messageleaderboard.json", "utf8"));
+
     };
     
     exports.conf = {
