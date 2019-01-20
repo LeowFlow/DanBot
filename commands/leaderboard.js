2e8f27fe412af5757889557b19c7b6f2729fa4cb
danielpmc <danielpd93@gmail.com>
2019-01-20 14:01:38 +0000

    Automatic - Date: 20/01/2019 - Time: 14:01:37.79

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const Discord = require("discord.js");
 let mlb = JSON.parse(fs.readFileSync("./datajsons/messageleaderboard.json", "utf8"));
+let msgss = mlb[message.author.id].msgs;
 exports.run = async (client, message, args, level) => {
     message.channel.send("", {
       embed: new Discord.RichEmbed()
       .setTitle("LeaderBoard-W.I.P.")
-      .setDescription(mlb[message.author.id].msgs)
+      .setDescription(msgss)
     })
   }
     exports.conf = {
