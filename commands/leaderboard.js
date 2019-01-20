db0064bf0532741fd782f4e8a5f8188ad1e523f5
danielpmc <danielpd93@gmail.com>
2019-01-20 13:38:46 +0000

    Automatic - Date: 20/01/2019 - Time: 13:38:45.21

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,15 +1,12 @@
 const fs = require("fs");
 let mlb = JSON.parse(fs.readFileSync("./datajsons/messageleaderboard.json", "utf8"));
 exports.run = async (client, message, args, level) => {
-    message.channel.send({
-		embed: {
-			author: { name: `DanBot's Message LeaderBoard!` },
-			description: `${mlb[message.author.id].msgs}`,
-			color: 0xFFFFFF
-		}
-	});
-    };
-    
+    message.channel.send("",{
+      embed: new Discord.RichEmbed;
+      .setTitle("This is the leaderboard command (W.I.P.)")
+      .setDescription(mlb[message.author.id].msgs)
+    });
+  }
     exports.conf = {
       enabled: true,
       guildOnly: false,
