da4a250469792b8251d7e59654a8b0f77c11215e
danielpmc <danielpd93@gmail.com>
2019-01-20 05:42:40 +0000

    Automatic - Date: 20/01/2019 - Time:  5:42:40.49

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -2,23 +2,6 @@ const fs = require("fs");
 exports.run = async (client, message, args, level) => {
   let coins = JSON.parse(fs.readFileSync("./datajsons/coins.json", "utf8"));
   let Coinss = coins[message.author.id].coins;
-  const { currency } = message.client;
-      const client = message.client
-      
-      return message.channel.send({
-            embed: {
-                color: 3447003,
-                title: "```\n== Top Five Global Users ==\n```",
-                //   title: "test\nsa",
-                description: currency.sort((a, b) => b.balance - a.balance)
-                    .filter(user => client.users.has(user.user_id))
-                    .first(5)
-                    .map((user, position) => `(${position + 1}) ${(client.users.get(user.user_id).tag)}: ${user.coins}`)
-                    .join('\n'),
-
-            }
-        })
-
     message.author.send({
 		embed: {
 			author: { name: `DanBot's Message LeaderBoard!` },
