73b672223f0ca6c73ec2eeaf4571a692e8c78c54
danielpmc <danielpd93@gmail.com>
2019-01-20 04:32:58 +0000

    Automatic - Date: 20/01/2019 - Time:  4:32:58.65

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 module.exports = (client, guild) => {
     client.logger.cmd(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`);
+    
     let Servericon = guild.iconURL;
     let guildEmbed = new Discord.RichEmbed()
     .setTitle("New Server!!!")
