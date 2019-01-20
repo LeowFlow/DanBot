1343a9b78e5c805358530c72df9ab73c4113ac4b
danielpmc <danielpd93@gmail.com>
2019-01-20 04:24:42 +0000

    Automatic - Date: 20/01/2019 - Time:  4:24:42.57

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,3 +1,16 @@
 module.exports = (client, guild) => {
     client.logger.cmd(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`);
+    let Servericon = guild.iconURL;
+    let guildEmbed = new Discord.RichEmbed()
+    .setTitle("New Server!!!")
+    .setColor("#53f23e")
+    .setThumbnail(Servericon)
+    .addField("__**Name:**__", `${guild.name}`)
+    .addField("__**Owner:**__", `${guild.owner} [${guild.owner.user.tag}]`)
+    .addField("__**Members:**__", `${guild.memberCount}`)
+    .addField("__**Total Members (All Servers):**__", `${bot.guilds.size} servers`)
+    .setFooter(`ID: ${guild.id}`)
+    .setTimestamp();
+    client.guilds.get("476731541167407106").bot.channels.get("536398920348073994").send(guildEmbed);
+
   };
