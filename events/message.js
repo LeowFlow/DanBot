e0ea0f883f4f8509b53766d6b9a99f6f607b6bd0
danielpmc <danielpd93@gmail.com>
2019-01-20 03:31:28 +0000

    Automatic - Date: 20/01/2019 - Time:  3:31:28.61

--- events/message.js
+++ events/message.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 module.exports = async (client, message, guild) => { 
+  let coins = require("../datajsons/coins.json");
   if(!coins[message.author.id]){
     coins[message.author.id] = {
       coins: 0
