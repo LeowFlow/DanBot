85ca189ace02285f326a17a4b2d0d0b823dbcc17
danielpmc <danielpd93@gmail.com>
2019-01-20 13:30:26 +0000

    Automatic - Date: 20/01/2019 - Time: 13:30:25.84

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ client.settings = new Enmap({name: "settings"});
 const sql = require("sqlite3")
 const db = new sql.Database('./database.db');
 
+client.on("message", async message => {
+  let mlb = JSON.parse(fs.readFileSync("./datajsons/messageleaderboard.json", "utf8"));
+  // Note for Dan: mlb = messageleaderboard this line ^^ will import the leaderboard 
+  if(!mlb[message.author.username]){//this will give us the name, if u want to use the name and the id in the leaderboard(dashboard) msg me ill have to change that line.
+     return mlb[message.author.username] ={
+          ID: message.author.id,   //this will get the userID
+          msgs: 1                  //this will start the message cout for this user *ik ur dumb :P so msg = messages*
+      }
+  }
+  mlb[message.author.username].msgs ++;
+  fs.writeFile("./datajsons/messageleaderboard.json", JSON.stringify(mlb), (err) => {
+   if (err) console.log(err)
+ });
+})
+
 //Loads Commands
 const init = async () => {
   const cmdFiles = await readdir("./commands/");
