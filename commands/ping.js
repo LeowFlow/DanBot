eb5f03656a23cae3049f66fc3a8b2db68f330c6e
danielpmc <danielpd93@gmail.com>
2019-01-20 06:12:15 +0000

    Automatic - Date: 20/01/2019 - Time:  6:12:15.46

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,9 @@ const Discord = require("discord.js");
 const ping = require('node-http-ping')
 var datetime = require('node-datetime');
 const Canvas = require('canvas')
-exports.run = async(client, message, args, color) => {
+exports.run = async (client, message, args, color) => {
+
+
     const canvas = Canvas.createCanvas(700, 250);
     const ctx = canvas.getContext('2d');
     const background = await Canvas.loadImage('./images/background.jpg');
@@ -15,7 +17,7 @@ exports.run = async(client, message, args, color) => {
     ctx.fillStyle = '#ffffff';
     ctx.fillText('Pong,', canvas.width / 2.5, canvas.height / 3.5);
   
-    ctx.font = applyText(canvas, `Latency: ${new Date().getTime() - message.createdTimestamp} ms`);
+    ctx.applyText(canvas, `Latency: ${new Date().getTime() - message.createdTimestamp} ms`);
     ctx.fillStyle = '#ffffff';
     ctx.fillText(`API: ${API}ms`, canvas.width / 2.5, canvas.height / 1.8);
   
@@ -24,13 +26,14 @@ exports.run = async(client, message, args, color) => {
     ctx.closePath();
     ctx.clip();
   
-    const { body: buffer } = snekfetch.get(member.user.displayAvatarURL);
-    const avatar = Canvas.loadImage(buffer);
+    const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
+    const avatar = await Canvas.loadImage(buffer);
     ctx.drawImage(avatar, 25, 25, 200, 200);
   
     const attachment = new Discord.Attachment(canvas.toBuffer(), 'PONG.png');
   
     channel.send(`PONG!!`, attachment);  
+
 };exports.conf = {
     enabled: true,
     guildOnly: true,
